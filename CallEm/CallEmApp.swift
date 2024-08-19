e07caeeaaeb86481eb97254cdba642b824e5a0c4
Farzan <farzanalif42@gmail.com>
2024-08-19 12:46:29 -0400

    Initial Commit

---
+++ CallEm/CallEmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallEmApp.swift
+//  CallEm
+//
+//  Created by Farzan Ali Faisal on 2024-08-19.
+//
+
+import SwiftUI
+
+@main
+struct CallEmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
